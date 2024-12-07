412f26b3524c8979847225aca4851bb5eff903e5
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-12-07 13:03:51 -0500

    Configuracion de las Taks con la expresion Lambda

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -19,8 +19,16 @@ namespace WPFApp
         public MainWindow()
         {
             InitializeComponent();
+            CreateTask();
         }
 
+
+        void AddMessage(string message)
+        {
+            Messages.Content +=
+                $"Mensaje: {message}," +
+                $"Hilo actual: {Thread.CurrentThread.ManagedThreadId}\n";
+        }
         void CreateTask()
         {
             Task T;
@@ -54,8 +62,11 @@ namespace WPFApp
             );
 
             Task T6 = new Task((message) =>
-            MessageBox.Show(message.ToString()), "Expresión Lambda con parametros");
+            MessageBox.Show(message.ToString()), "Expresión Lambda con parametros.");
 
+            Task T7 = new Task(() => AddMessage("Ejecutando la Tarea"));
+            T7.Start();
+            AddMessage("En el hilo principal");
         }
 
         void ShowMessege()
